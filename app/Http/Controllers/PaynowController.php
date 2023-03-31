99a27331ac2ded05fff465b958b098164f8c7424
Samuel Jarai <jarai.samuel@gmail.com>
2023-03-31 18:05:47 +0200

    started working on paynow

--- app/Http/Controllers/PaynowController.php
+++ app/Http/Controllers/PaynowController.php
@@ -2,9 +2,68 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Services\PaynowService;
+use App\Models\Payment;
 use Illuminate\Http\Request;
 
 class PaynowController extends Controller
 {
+
+    public $service;
+
+    public function __construct()
+    {
+        $this->service = new PaynowService();
+    }
+
+    public function mobilePayment(Request $request)
+    {
+        try {
+            if ($this->service->makeMobilePayment($request->uniqueId, $request->email, $request->phone, $request->network, $request->order)) {
+                return response()->json(['message' => 'success']);
+            } else {
+                return response()->json(['message' => 'invalid order']);
+            }
+        } catch (\Exception $e) {
+            return response()->json(['message' => 'failed']);
+        }
+    }
+
+    public function bankPayment(Request $request)
+    {
+        try {
+            if ($this->service->makeBankPayment($request->uniqueId, $request->email, $request->order)) {
+                return response()->json(['message' => 'success']);
+            } else {
+                return response()->json(['message' => 'invalid order']);
+            }
+        } catch (\Exception $e) {
+            return response()->json(['message' => 'failed']);
+        }
+
+    }
+
+    public function paymentStatus(Request $request)
+    {
+        try {
+            $result = Payment::where('reference', $request->uniqueId)->first();
+            return response()->json(['message' => 'success', 'payment' => $result]);
+        } catch (\Exception $e) {
+            return response()->json(['message' => 'failed']);
+        }
+
+    }
+
+    public function handleResult(Request $request)
+    {
+        $payment = new Payment();
+        $payment->reference = $request['reference'];
+        $payment->reference = $request['paynowreference'];
+        $payment->status = $request['status'];
+        $payment->poll_url = $request['pollurl'];
+        $payment->save();
+        return response('', 200);
+    }
+
 
 }
--- app/Http/Services/PaynowService.php
+++ app/Http/Services/PaynowService.php
@@ -1,6 +1,65 @@
 <?php
 
 namespace App\Http\Services;
+use App\Models\Painting;
+use App\Models\WhatsappSetting;
+use Paynow\Payments\Paynow;
+
 class PaynowService{
+
+    public $id = '15485';
+    public $key = 'df033924-f9bb-4056-bc77-934657ee2ab1';
+    public $returnUrl = 'https://c506-104-223-93-213.ngrok.io/paynow/return';
+    public  $resultUrl = 'https://c506-104-223-93-213.ngrok.io/api/paynow/result';
+
+
+    public function __construct()
+    {
+        $this->paynow = new Paynow($this->id,$this->key,$this->returnUrl,$this->resultUrl);
+    }
+
+
+    public function makeMobilePayment($uniqueId,$email,$phone,$network,$order)
+    {
+        if($this->verifyOrder($order)){
+            $payment=$this->paynow->createPayment($uniqueId,$email);
+            foreach($order as $item){
+                $payment->add($item->name,$item->price);
+            }
+            $response = $this->paynow->sendMobile($payment, $phone, $network);
+            return true;
+        }
+        else{
+            return false;
+        }
+
+
+    }
+
+    public function makeBankPayment($uniqueId,$email,$order)
+    {
+        if($this->verifyOrder($order)){
+            $payment=$this->paynow->createPayment($uniqueId,$email);
+            foreach($order as $item){
+                $payment->add($item->name,$item->price);
+            }
+            $response = $this->paynow->send($payment);
+            return true;
+        }
+        return false;
+
+    }
+
+    public function verifyOrder($order)
+    {
+        foreach($order as $item){
+            if(!Painting::find($item->id)->status=='available'){
+                return false;
+            }
+        }
+        return true;
+    }
+
+
 
 }
